564194d9257de125756941cad8b095d9df603051
alanzhang03 <alan.s.zhang@gmail.com>
2024-01-30 16:31:24 -0500

    css linked

--- tftAPP/tftAPP/settings.py
+++ tftAPP/tftAPP/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # Celery settings
 CELERY_BROKER_URL = "redis://localhost:6379"
 CELERY_RESULT_BACKEND = "redis://localhost:6379"
